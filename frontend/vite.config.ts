78b74ad9fbecbc0c4d8533468d08187d6f71f73a
Michael Rizhakov <m.rizhakov@gmail.com>
2025-12-09 01:02:20 +0100

    Switched to pre-generated ssl certs for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,25 @@
 import { defineConfig } from "vite";
-import basicSsl from "@vitejs/plugin-basic-ssl";
-console.log("VITE_API_BASE from process.env:", process.env.VITE_API_BASE);
+import fs from "fs";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [basicSsl()],
 	server: {
 		host: "0.0.0.0",
 		port: 5173,
-		// https: true,
+		https: {
+			key: fs.readFileSync("/run/secrets/frontend_tls_key"), // or a bind-mounted path
+			cert: fs.readFileSync("/run/secrets/frontend_tls_cert"),
+		},
 	},
 });
+
+// import basicSsl from "@vitejs/plugin-basic-ssl";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+// 	plugins: [basicSsl()],
+// 	server: {
+// 		host: "0.0.0.0",
+// 		port: 5173,
+// 		// https: true,
+// 	},
+// });
